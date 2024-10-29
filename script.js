249ca3abbc893c5bdad97345ea417a1f644f6e84
nik-89nur <karpusenkonikita21@gmail.com>
2024-10-29 17:10:08 +0500

    кракозябра

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+
+// let name = "Арнольд"
+// for (i=0; i<7; i++){
+//     if (name[i] == "А" || name[i] == "о")
+//     {/* то ничего */}
+//     else{
+//         console.log(name[i])
+//     }
+// }
+
+// let num = 20 
+// for (i=1; i<num; i++){
+//     if (i%3 == 0)
+//         console.log(i)
+// }
+
+
+
+
+// let stat = ""
+// for (i=1; i<6; i++){
+//     stat = stat + i 
+//     console.log(stat)
+// }
+
+
+// let num3 = 4
+// let sum = 0 
+// for(let n = 1; n<=num3; n++){
+//     let fact = 1;
+//     for(let p = 1; p<=n; p++){
+//         fact = fact*p;
+//     }
+//     sum = sum+fact;
+// }
+// console.log(sum)
